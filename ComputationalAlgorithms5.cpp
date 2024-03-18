0fd5df8cd90d79ecc4b1c98a0aca57c8bb42afb4
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-03-18 23:42:00 +0700

    Переписаны функции для вычисления математических функций

--- ComputationalAlgorithms5.cpp
+++ ComputationalAlgorithms5.cpp
@@ -3,51 +3,46 @@
 #include "exprtk.hpp"
 #include <vector>
 
-std::vector<std::vector<double>> GetTableByFunction(std::vector<double> Arguments, std::string ExpressionString);
-std::vector<exprtk::expression<double>> MakeExpressionTable(std::vector<exprtk::symbol_table<double>> SymbolTable,
-    std::string ExpressionString, int Size);
+double GetFunctionValue(double Argument, std::string ExpressionString);
+exprtk::expression<double> MakeExpression(exprtk::symbol_table<double> SymbolTable, std::string ExpressionString);
+double LeftRectangleMethod(double BeginOfIntegration, double EndOfIntegration, int NumberOfAmounts);
+double TrapezoidMethod(double BeginOfIntegration, double EndOfIntegration, int NumberOfAmounts);
+double SimpsonMethod(double BeginOfIntegration, double EndOfIntegration, int NumberOfAmounts);
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    std::cout << "Введите функцию: ";
+    std::string Function;
+    std::cin >> Function;
+    std::cout << GetFunctionValue(0.5, Function);
     return 0;
 }
 
 //Получает таблицу по введенной в аналитическом виде функции
-std::vector<std::vector<double>> GetTableByFunction(std::vector<double> Arguments, std::string ExpressionString)
+double GetFunctionValue(double Argument, std::string ExpressionString)
 {
-    std::vector<exprtk::symbol_table<double>> SymbolTable(Arguments.size());
-    for (int i = 0; i < Arguments.size(); i++) { //Заполнение таблицы символов
-        SymbolTable[i].add_variable("x", Arguments[i]);
-        SymbolTable[i].add_function("abs", abs);
-        SymbolTable[i].add_function("sqrt", sqrt);
-        SymbolTable[i].add_function("exp", exp);
-    }
-    //Создаются выражения, вычисляются значения функции
-    std::vector<exprtk::expression<double>> Expression = MakeExpressionTable(SymbolTable, ExpressionString, Arguments.size());
-    std::vector<std::vector<double>> Table(2, std::vector<double>(Arguments.size()));
-    // Заполняет таблицу аргументами и значениями функции
-    for (int i = 0; i < Arguments.size(); i++)
-    {
-        Table[0][i] = Arguments[i];
-        Table[1][i] = Expression[i].value();
-    }
-    return Table;
+    exprtk::symbol_table<double> SymbolTable;
+    //Заполнение таблицы символов
+    SymbolTable.add_variable("x", Argument);
+    SymbolTable.add_function("abs", abs);
+    SymbolTable.add_function("sqrt", sqrt);
+    SymbolTable.add_function("exp", exp);
+    //Создается выражение, вычисляется значение функции
+    exprtk::expression<double> Expression = MakeExpression(SymbolTable, ExpressionString);
+    return (double)Expression;
 }
 
 //Создает выражения, вычисляются значения функции
-std::vector<exprtk::expression<double>> MakeExpressionTable(std::vector<exprtk::symbol_table<double>> SymbolTable,
-    std::string ExpressionString, int Size)
+exprtk::expression<double> MakeExpression(exprtk::symbol_table<double> SymbolTable, std::string ExpressionString)
 {
     //Создается вектор выражений заданного размера
-    std::vector<exprtk::expression<double>> Expression(Size);
-    for (int i = 0; i < Size; i++)
-        Expression[i].register_symbol_table(SymbolTable[i]);
-    //Компилируется каждое выражение с помощью парсера
+    exprtk::expression<double> Expression;
+    Expression.register_symbol_table(SymbolTable);
+    //Компилируется выражение с помощью парсера
     exprtk::parser<double> Parser;
-    for (int i = 0; i < Size; i++)
-        if (!Parser.compile(ExpressionString, Expression[i]))
-            std::cout << "Ошибка в выражении!\n";
-    return Expression; //Возвращается вектор скомпилированных выражений
+    if (!Parser.compile(ExpressionString, Expression))
+        std::cout << "Ошибка в выражении!\n";
+    return Expression; //Возвращается выражение
 }
